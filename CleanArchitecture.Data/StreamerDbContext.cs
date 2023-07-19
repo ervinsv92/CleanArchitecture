efe202e86633964b8713576a7d5ecf5b76d4226a
ervinsv92 <ervinsv92@gmail.com>
2023-07-18 22:36:05 -0600

    Agregar archivos de proyecto.

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress; Initial Catalog=Streamer; Integrated Security=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
---
+++ CleanArquitecture.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+
+StreamerDbContext dbContext = new StreamerDbContext();
+
+//await AddNewRecords();
+QueryStreaming();
+
+void QueryStreaming (){
+    var streamers = dbContext!.Streamers!.ToList();
+    foreach(var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+}
+
+async Task AddNewRecords() {
+    Streamer streamer = new Streamer() { Nombre = "Disney", Url = "https://www.disney.com" };
+
+    dbContext!.Streamers!.Add(streamer);
+
+    await dbContext.SaveChangesAsync();
+
+    var movies = new List<Video>
+{
+    new Video
+    {
+        Nombre="La Cenicienta",
+        StreamerId=streamer.Id
+    },
+    new Video
+    {
+        Nombre="1001 Dalmatas",
+        StreamerId=streamer.Id
+    },
+    new Video
+    {
+        Nombre="El Jorobado de Notredame",
+        StreamerId=streamer.Id
+    },
+    new Video
+    {
+        Nombre="Star Wars",
+        StreamerId=streamer.Id
+    },
+};
+
+    await dbContext.AddRangeAsync(movies);
+    await dbContext.SaveChangesAsync();
+}
