d2fcd0bc0eabff5aa6c6c7a8848649e7d6f2264c
ervinsv92 <ervinsv92@gmail.com>
2023-07-20 21:33:42 -0600

    Hasta clase 17

--- CleanArchitecture.Data/StreamerDbContext.cs
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -13,7 +13,17 @@ namespace CleanArchitecture.Data
                 .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
                 .EnableSensitiveDataLogging();
         }
+
+        //FluentAPI relacion de uno a muchos/muchos a muchos, es opcional si se siguen las convenciones de los nombres de los atributos de los objetos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>().HasMany(m => m.Videos).WithOne(m => m.Streamer).HasForeignKey(m => m.StreamerId).IsRequired().OnDelete(DeleteBehavior.Restrict);//para la llave de uno a muchos
+
+            modelBuilder.Entity<Video>().HasMany(p => p.Actores).WithMany(t => t.Videos).UsingEntity<VideoActor>(pt => pt.HasKey(e => new { e.ActorId, e.VideoId }));//para la llave de muchos a muchos
+        }
         public DbSet<Streamer>? Streamers { get; set; }
         public DbSet<Video>? Videos { get; set; }
+        public DbSet<Actor>? Actor { get; set; }
+        public DbSet<Director>? Director { get; set; }
     }
 }
